28e3f85aa9a8e828c23cb3efe8617f723b9f6d70
st25161-rgb <st25161@ormiston.school.nz>
2026-05-10 20:18:58 +1200

    json stuff

---
+++ app.py
@@ -0,0 +1,21 @@
+import json
+
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+def load_data():
+    with open('data/flowers.json') as file:
+        flowers = json.load(file)
+    return flowers
+
+@app.route('/')
+def index():
+    flowers = load_data()
+    return render_template('index.html', flowers=flowers)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

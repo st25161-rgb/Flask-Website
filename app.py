683d48b4384bd0481fb8acf7207d8fd1210a68eb
st25161-rgb <st25161@ormiston.school.nz>
2026-05-12 09:53:29 +1200

    addons.json aint wokring will figure that out since the '/' route isnt routing

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
+
+def load_data():
+    with open('data/addons.json') as file:
+        addons = json.load(file)
+    return addons
+
 def load_data():
     with open('data/flowers.json') as file:
         flowers = json.load(file)
@@ -12,8 +18,31 @@ def load_data():
 
 @app.route('/')
 def index():
+    addons = load_data()
+    return render_template("index.html", addon=addons)
+
+@app.route('/')
+def index():
     flowers = load_data()
-    return render_template('index.html', flowers=flowers)
+    return render_template("index.html", flower=flowers)
+
+
+
+
+
+# @app.route('/about')
+# def about():
+#     return render_template('about.html')
+
+
+# @app.route('/checkout')
+# def checkout():
+#     return render_template('invoice.html')
+
+
+# @app.route('/orders')
+# def order_history():
+#     return render_template('order_history.html')
 
 
 
--- app1.py
+++ app1.py
@@ -1,12 +1,20 @@
+import json
+
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
+def load_data():
+    with open('data/flowers.json') as file:
+        flowers = json.load(file)
+    return flowers
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    flowers = load_data()
+    return render_template('index.html', flower=flowers)
 
 
 if __name__ == '__main__':

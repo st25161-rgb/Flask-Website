a40a3f1d27d9d6b0b822cc8c74c71fd7113bbb99
st25161-rgb <st25161@ormiston.school.nz>
2026-05-12 22:34:29 +1200

    add to cart button aint working, up to the form part

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+# @app.route('/about')
+# def about():
+#     return render_template('about.html')
+
+
+# @app.route('/checkout')
+# def checkout():
+#     return render_template('invoice.html')
+
+
+# @app.route('/orders')
+# def order_history():
+#     return render_template('order_history.html')
+
+
 import json
 
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, session, flash, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -18,27 +33,41 @@ def load_data():
 
 @app.route('/')
 def index():
-    addons = load_data()
     flowers = load_data()
+    addons = load_data()
     return render_template("index.html", addon=addons, flower=flowers)
 
+# Info on form data
+@app.route('/add_to_cart', methods=["POST"])
+def add_to_cart():
+    flower = request.form[flower] #flower name
+    quantity = int(request.form['quantity']) #quantity in numbers
+    flowers, addons = load_data() #aqquires flower data - the addon data
+    cart = session.get('cart', {}) # get cart from the session or a fresh one
+
+    if flower not in flowers:
+        flash("invalid flower selected.")
+        return redirect(url_for('home')) 
+    
+    if flower in cart:
+        cart[flower] [quantity] += quantity #add existing quantity
+    else:
+        cart[flower] = {
+            'price': flower[flower]['price'],
+            'quantity': quantity
+        }
+
+    session ['cart'] = cart # update the session
+    session.modified = True # flask will save the data
+    flash(f"{quantity} {flower}(s) added to cart.") #says the [flower name] [amount] is added to the end users cart
+    return redirect(url_for('home'))
 
 
-# @app.route('/about')
-# def about():
-#     return render_template('about.html')
 
-
-# @app.route('/checkout')
-# def checkout():
-#     return render_template('invoice.html')
-
-
-# @app.route('/orders')
-# def order_history():
-#     return render_template('order_history.html')
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+

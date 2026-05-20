64c76ad31cbefe92147633403dd98d9aae94fe63
st25161-rgb <st25161@ormiston.school.nz>
2026-05-20 20:44:33 +1200

    styling html files

--- app.py
+++ app.py
@@ -5,62 +5,90 @@ from flask import Flask, render_template, request, session, flash, redirect, url
 app = Flask(__name__)
 app.secret_key = '11037'
 
-
-def load_data():
+def load_addon_data():
     with open('data/addons.json') as file:
         addons = json.load(file)
     return addons
     
-def load_data():
+def load_flower_data():
     with open('data/flowers.json') as file:
         flowers = json.load(file)
     return flowers
 
 @app.route('/')
 def index():
-    flowers = load_data()
-    addons = load_data()
-    return render_template("index.html", 
-                           flower=flowers, 
-                           addon=addons)
+    flowers = load_flower_data()
+    addons = load_addon_data()
+    cart = session.get('cart', {})
+    return render_template("index.html", flower=flowers, addon=addons, cart=cart)
 
 
 @app.route('/index1')
 def index1():
-    flowers = load_data()
-    addons = load_data()
-    return render_template("index.html", 
-                           flower=flowers, 
-                           addon=addons)  
+    flowers = load_flower_data()
+    addons = load_addon_data()
+    return render_template("index1.html", flower=flowers, addon=addons)  
+
+@app.route('/base')
+def base():
+    
+    return render_template("base.html") 
+
+@app.route('/invoice')
+def invoice():
+    
+    return render_template("invoice.html")  
+
+@app.route('/order_history')
+def order_history():
+    
+    return render_template("order_history.html") 
+
+@app.route('/about')
+def about():
+    
+    return render_template("about.html") 
 
 # Info on form data
 @app.route('/add_to_cart', methods=["POST"])
 def add_to_cart():
     flower = request.form['flower'] #grab flower name
     quantity = int(request.form['quantity']) # makes the quantity a number
-    flowers = load_data() #gets flower data from file
+    flowers = load_flower_data() #gets flower data from file
     cart = session.get('cart', {}) #gets a cart from session or starts a fresh one
 
     if flower not in flowers:
         flash("invalid flower selected")
-        return redirect(url_for('/'))
+        return redirect(url_for('index'))
     
     if flower in cart:
-        cart[flower][quantity] += quantity # adds existing quantity
+        cart[flower]['quantity'] += quantity # adds existing quantity
     else:
         cart[flower] = {
             'price': flowers[flower]['price'],
-            'quantity': quantity
+            'quantity': quantity 
+            }
 
-        }
-
-    session['cart'] = cart #upadtes session
+    session['cart'] = cart #updates session
     session.modified = True #flask will save it
-    flash(f"{quantity} {flowers}(s) added to cart") #message sent to end user upon action
+    flash(f"{quantity} {flower}(s) added to cart") #message sent to end user upon action
     return redirect(url_for('index')) #refreshes homepage
 
-with open("data/addons.json", "r") as f:
-    data = json.load(f)
+@app.route('/remove_from_cart/<item>')
+def remove_from_cart(item):
+    cart = session.get('cart', {})
+
+    if item in cart:
+        del cart[item]
+        session['cart'] = cart
+        session.modified = True
+        flash(f"{item} removed from cart.")
+    else:
+        flash(f"{item} not found in cart.")
+         
+    return redirect(url_for('index'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
